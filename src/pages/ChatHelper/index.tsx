5c77c90a5de4f7575dc88de924f66ac7c495fb12
hariprasathys22 <confluenceinfotechsolutions@gmail.com>
2025-02-14 08:51:00 +0530

    Add project selection dropdown to ChatHelper; implement query handling based on selected project

--- src/pages/ChatHelper/index.tsx
+++ src/pages/ChatHelper/index.tsx
@@ -2,53 +2,86 @@ import { IconButton, TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { GoArrowRight } from "react-icons/go";
 import { useQueryStore } from "../../store";
+import DropDownComponent from "../../components/DropDown";
+import { ServerConfig } from "../../utilities/baseConfig";
 
 const ChatHelper = () => {
   const [queryData, setQueryData] = useState("");
+  const [dropDownProject, setDropDownProject] = useState("");
   const [queryInput, setQueryInput] = useState("");
-  const { fetchProject, projects } = useQueryStore()
+  const { fetchProject, projects } = useQueryStore();
 
   const handleQueryChange = (e: any) => {
     setQueryInput(e.target.value);
   };
-  console.log(projects, "new")
+
+  const handleProjectChange = (event: any) => {
+    setDropDownProject(event.target.value);
+  };
+
+  const fetchQueryResponse = async () => {
+    const response = await fetch(`${ServerConfig.BASE_URL}api/querytext/${dropDownProject}`, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json", // Add this header
+      },
+      body: JSON.stringify({query: queryInput})
+    })
+    const data = await response.json()
+    if(response.ok){
+      setQueryData(data.message)
+      console.log(data);
+      
+    }
+
+  }
 
   useEffect(() => {
     fetchProject();
   }, []);
 
   return (
-    <div className="w-full h-full p-4 flex justify-center">
-      <div className="w-full"></div>
+    <div className="w-full h-full p-4 ">
+      <div className="w-1/4 relative">
+        <DropDownComponent
+          handleChange={handleProjectChange}
+          content={projects}
+          value={dropDownProject} // <-- Pass the state you want to bind
+          label="Select Project"
+        />
+      </div>
       <div className="w-1/2 h-auto">
         {queryData && <span>{queryData}</span>}
       </div>
-      <div className="w-1/2 flex justify-between items-center bottom-10 fixed gap-10">
-        <TextField
-          size="medium"
-          variant="outlined" // Ensure you're using the outlined variant if you want to style the outline
-          placeholder="What information do you love to hear today?"
-          onChange={(e) => handleQueryChange(e)}
-          sx={{
-            width: "90%",
-            "& .MuiOutlinedInput-root": {
-              borderRadius: "40px" // Set your desired border radius here
-            }
-          }}
-        />
-        <div className="w-auto">
-          <IconButton
+      <div className="w-full flex justify-center items-center">
+        <div className="w-1/2 flex justify-center items-center bottom-10 fixed gap-10">
+          <TextField
+            size="medium"
+            variant="outlined" // Ensure you're using the outlined variant if you want to style the outline
+            placeholder="What information do you love to hear today?"
+            onChange={(e) => handleQueryChange(e)}
             sx={{
-              width: "56px",
-              height: "56px",
-              backgroundColor: "#ef6a36",
-              "&:hover": {
-                backgroundColor: "#24252d" // Optional: change background on hover
-              }
+              width: "90%",
+              "& .MuiOutlinedInput-root": {
+                borderRadius: "40px", // Set your desired border radius here
+              },
             }}
-          >
-            <GoArrowRight style={{ fontSize: "50px", color: "#ffffff" }} />
-          </IconButton>
+          />
+          <div className="w-auto">
+            <IconButton
+              sx={{
+                width: "56px",
+                height: "56px",
+                backgroundColor: "#ef6a36",
+                "&:hover": {
+                  backgroundColor: "#24252d", // Optional: change background on hover
+                },
+              }}
+              onClick={fetchQueryResponse}
+            >
+              <GoArrowRight style={{ fontSize: "50px", color: "#ffffff" }} />
+            </IconButton>
+          </div>
         </div>
       </div>
     </div>
